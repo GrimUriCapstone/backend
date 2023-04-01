785958cf54fc29c1ef1e278caba20a2b8690c671
BambooKim <rlaqjarn1008@gmail.com>
2023-04-01 22:31:04 +0900

    Initial Commit

---
+++ src/main/java/grimuri/backend/GrimUriBackendApplication.java
@@ -0,0 +1,13 @@
+package grimuri.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimUriBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrimUriBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grimuri/backend/GrimUriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package grimuri.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimUriBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
