e157a329e184feafb1632d6c5ec5e6610adbccf9
BambooKim <rlaqjarn1008@gmail.com>
2023-04-03 23:12:49 +0900

    feat: Entity 추가 #1 - User, Diary, Image - BaseTimeEntity

---
+++ src/main/java/grimuri/backend/domain/image/Image.java
@@ -0,0 +1,27 @@
+package grimuri.backend.domain.image;
+
+import grimuri.backend.domain.BaseTimeEntity;
+import grimuri.backend.domain.diary.Diary;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Image extends BaseTimeEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "image_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String sourceUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id", nullable = false)
+    private Diary diary;
+
+}
