2b69ac649b5bf63137f8f23cd1cb0df317aa9174
BambooKim <rlaqjarn1008@gmail.com>
2023-04-04 14:33:26 +0900

    feat: Controller, Service 추가

---
+++ src/main/java/grimuri/backend/domain/user/UserController.java
@@ -0,0 +1,15 @@
+package grimuri.backend.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/diary")
+public class UserController {
+
+
+}
