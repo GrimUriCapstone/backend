55bc96273148f6efd4ae955ab585aea1024eaa06
Beomgoo Kim <rlaqjarn1008@gmail.com>
2023-04-04 20:38:48 +0900

    [Resolves #4] 후보 이미지 목록 조회 API 구현 (#5)
    * feat: 후보 이미지 목록 조회
    #4
    
    * chore: DiaryService.getCandidateImageList Description
    #4

--- src/main/java/grimuri/backend/domain/diary/DiaryRepository.java
+++ src/main/java/grimuri/backend/domain/diary/DiaryRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    Boolean existsByIdAndSelectedTrue(Long diaryId);
+
 }
