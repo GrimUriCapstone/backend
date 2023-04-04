202de8a3fad09f39290617a7f98249cdbd925cdc
BambooKim <rlaqjarn1008@gmail.com>
2023-04-04 09:38:56 +0900

    feat: Repository 추가
    - UserRepository
    - DiaryRepository
    - ImageRepository

---
+++ src/main/java/grimuri/backend/domain/image/ImageRepository.java
@@ -0,0 +1,8 @@
+package grimuri.backend.domain.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
