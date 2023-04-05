6aa75244cb201a69ed1a42732931076782246ecc
Beomgoo Kim <rlaqjarn1008@gmail.com>
2023-04-05 18:11:58 +0900

    feat: 일기 생성 API (#13)
    #11
    - DiaryController.createDiary
    - DiaryRequestDto.Create
    - DiaryResponseDto.Create
    - DiaryService.createDiary
    - EnableJpaAudit

--- src/main/java/grimuri/backend/GrimUriBackendApplication.java
+++ src/main/java/grimuri/backend/GrimUriBackendApplication.java
@@ -2,8 +2,10 @@ package grimuri.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GrimUriBackendApplication {
 
     public static void main(String[] args) {
