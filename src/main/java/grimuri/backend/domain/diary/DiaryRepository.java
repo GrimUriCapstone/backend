5b475cf82c87a9bb8ed69b3b091e4868c49ef31b
Beomgoo Kim <rlaqjarn1008@gmail.com>
2023-04-05 18:08:09 +0900

    feat: Diary 대표 이미지 액세스 (#12)
    #8
    diaryId 정보와 함께 대표 이미지 액세스를 요청하면, 해당 diaryId의 대표 이미지 URL을 조회하여 Redirect 시킴.
    - Diary.imageList : @BatchSize(10) 추가
    - DiaryController.redirectToMainImage
    - DiaryService.getMainImageUrl

--- src/main/java/grimuri/backend/domain/diary/DiaryRepository.java
+++ src/main/java/grimuri/backend/domain/diary/DiaryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
@@ -14,5 +15,8 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     List<Diary> findByUser_Seq(Long userSeq);
 
+    Optional<Diary> findById(Long diaryId);
+
     Page<Diary> findByUser_Seq(Long userSeq, Pageable pageable);
+
 }
