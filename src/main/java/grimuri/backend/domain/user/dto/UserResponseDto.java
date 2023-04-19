2bd4f1191d7a81a00ab9f4598e0e71e453ef9878
Beomgoo Kim <rlaqjarn1008@gmail.com>
2023-04-19 12:15:48 +0900

    [Resolves #28] 회원 정보 조회 시 사용자의 프로필 이미지 추가
    #28

--- src/main/java/grimuri/backend/domain/user/dto/UserResponseDto.java
+++ src/main/java/grimuri/backend/domain/user/dto/UserResponseDto.java
@@ -14,6 +14,31 @@ public class UserResponseDto {
     @AllArgsConstructor
     @Builder
     @ToString
+    @Schema(description = "회원가입 후 반환 정보")
+    public static class AfterSignup {
+
+        @Schema(description = SchemaDescriptionUtils.AfterSignup.username)
+        private String username;
+
+        @Schema(description = SchemaDescriptionUtils.AfterSignup.email)
+        private String email;
+
+        @Schema(description = SchemaDescriptionUtils.AfterSignup.nickname)
+        private String nickname;
+
+        public static AfterSignup of(User user) {
+            return AfterSignup.builder()
+                    .username(user.getUsername())
+                    .email(user.getEmail())
+                    .nickname(user.getNickname())
+                    .build();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    @ToString
     @Schema(description = "사용자 정보")
     public static class UserInfo {
 
@@ -26,11 +51,15 @@ public class UserResponseDto {
         @Schema(description = SchemaDescriptionUtils.UserInfo.nickname)
         private String nickname;
 
-        public static UserInfo of(User user) {
+        @Schema(description = SchemaDescriptionUtils.UserInfo.profileImage)
+        private String profileImage;
+
+        public static UserInfo of(User user, String profileImage) {
             return UserInfo.builder()
                     .username(user.getUsername())
                     .email(user.getEmail())
                     .nickname(user.getNickname())
+                    .profileImage(profileImage)
                     .build();
         }
     }
