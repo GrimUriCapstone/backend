93f8777bbc26e41ff2317cc3da9a3a3bbc182e1d
BambooKim <rlaqjarn1008@gmail.com>
2023-06-02 18:14:41 +0900

    fix: Redisson Client 설정을 하지 않을 때 나타나는 오류 해결

--- src/main/java/grimuri/backend/global/config/RedisConfig.java
+++ src/main/java/grimuri/backend/global/config/RedisConfig.java
@@ -3,30 +3,50 @@ package grimuri.backend.global.config;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.grid.jcache.JCacheProxyManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
 import org.redisson.jcache.configuration.RedissonConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import javax.cache.CacheManager;
 import javax.cache.Caching;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class RedisConfig {
 
-    private final Environment env;
+    @Value("${spring.redis.host}")
+    private String REDIS_HOST;
+
+    @Value("${spring.redis.port}")
+    private String REDIS_PORT;
+
+    @Value("${spring.redis.password}")
+    private String REDIS_PASSWORD;
+
+    private final static String PREFIX = "redis://";
 
     @Bean
     public Config config() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress(env.getProperty("REDIS_ADDRESS"))
-                .setPassword(env.getProperty("REDIS_PASSWORD"));
+                .setAddress(PREFIX + REDIS_HOST + ":" + REDIS_PORT)
+                .setPassword(REDIS_PASSWORD);
 
         return config;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = Redisson.create(this.config());
+
+        return redisson;
     }
 
     @Bean(name = "SpringCM")
